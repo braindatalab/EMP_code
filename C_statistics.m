5da48dc621b6ac290b5f588f55284196d4a17544
vss245 <16902414+vss245@users.noreply.github.com>
2022-05-13 11:24:45 +0200

    final fixes

--- C_statistics.m
+++ C_statistics.m
@@ -1,4 +1,4 @@
-%% perform statistical analysis 
+%% perform statistical analysis
 % Authors: V. Shamova, S. Haufe, N. Agarwal
 set_paths
 fprintf('loading data... \n')
@@ -32,25 +32,24 @@ for i=1:size(subs,1)
     avg_data = squeeze(mean(mean(data_sub,1),2));
     [n1_amp,n1_idx] = min(avg_data(n1_range(1):n1_range(2)));
     n1_time = n1_idx+n1_range(1)-1;
-    % get amplitude for this time 
+    % get amplitude for this time
     n1_manmade = squeeze(data_sub(:,n1_time,trial_ind(i).man))';
-    n1_natural = squeeze(data_sub(:,n1_time,trial_ind(i).nat))'; 
-    % effect of N1 (mean and var) 
+    n1_natural = squeeze(data_sub(:,n1_time,trial_ind(i).nat))';
+    % effect of N1 (mean and var)
     % mean difference
-    ds(i,:) = mean(n1_manmade,1) - mean(n1_natural,1);  
-    % variance 
-    v_1 = var(n1_manmade,0,1);  
-    v_2 = var(n1_natural,0,1); 
-    vars(i, :) = v_1 ./ size(n1_manmade,1) + v_2 ./ size(n1_natural,1); 
+    ds(i,:) = mean(n1_manmade,1) - mean(n1_natural,1);
+    % variance
+    v_1 = var(n1_manmade,0,1);
+    v_2 = var(n1_natural,0,1);
+    vars(i, :) = v_1 ./ size(n1_manmade,1) + v_2 ./ size(n1_natural,1);
 end
 [p_n1_uncorr1, p_n1_fdr1, z_re1] = group_analysis(ds, vars,num_channels,alpha);
 save([p_path 'p_val_H1.mat'],'p_n1_uncorr1','p_n1_fdr1', 'z_re1');
-%plot_topomap(-log10(p_n1_fdr1));
 %% Hypothesis 2
 hypothesis = 2;
 fprintf('hypothesis %d\n', hypothesis)
 % old vs new images
-% EEG voltage at fronto-central channels 
+% EEG voltage at fronto-central channels
 % alpha at posterior channels
 % theta at fronto-central channels
 % get channel indices
@@ -75,20 +74,20 @@ for i = 1:size(subs,1)
     alpha_pow_old = squeeze(mean(data_sub_power(trial_ind(i).old,post_chans,f>=8&f<13,windows),3));
     alpha_pow_new = squeeze(mean(data_sub_power(trial_ind(i).new,post_chans,f>=8&f<13,windows),3));
     theta_pow_old = squeeze(mean(data_sub_power(trial_ind(i).old,fc_chans,f>=4&f<7,windows),3));
-    theta_pow_new = squeeze(mean(data_sub_power(trial_ind(i).new,fc_chans,f>=4&f<7,windows),3)); 
+    theta_pow_new = squeeze(mean(data_sub_power(trial_ind(i).new,fc_chans,f>=4&f<7,windows),3));
     % effect sizes calculation (mean and var)
     % voltage
-    voltage_new_old_ds(i,:,:) = mean(voltage_new,1) - mean(voltage_old,1);  
+    voltage_new_old_ds(i,:,:) = mean(voltage_new,1) - mean(voltage_old,1);
     voltage_new_old_vars(i,:,:) = var(voltage_new,0,1)./ size(voltage_new,1)+ ...
-        var(voltage_old,0,1) ./ size(voltage_old,1);    
+        var(voltage_old,0,1) ./ size(voltage_old,1);
     % theta
     theta_ds(i,:,:) = mean(theta_pow_new,1) - mean(theta_pow_old,1);  % mean difference of both classes per subject
     theta_vars(i,:,:) = var(theta_pow_new,0,1) ./ size(theta_pow_new,1) + ...
-        var(theta_pow_old,0,1) ./ size(theta_pow_old,1); 
+        var(theta_pow_old,0,1) ./ size(theta_pow_old,1);
     % alpha
     alpha_ds(i,:,:) = mean(alpha_pow_new,1) - mean(alpha_pow_old,1);  % mean difference of both classes per subject
     alpha_vars(i,:,:) = var(alpha_pow_new,0,1) ./ size(alpha_pow_new,1) + ...
-        var(alpha_pow_old,0,1) ./ size(alpha_pow_old,1); 
+        var(alpha_pow_old,0,1) ./ size(alpha_pow_old,1);
 end%%load files
 [p_volt_uncorr2, p_volt_fdr2, z_volt2] = group_analysis(voltage_new_old_ds,voltage_new_old_vars,length(fc_chans),alpha);
 [p_alpha_uncorr2, p_alpha_fdr2, z_alpha2]=group_analysis(alpha_ds,alpha_vars,length(post_chans),alpha);
@@ -115,13 +114,13 @@ for i = 1:size(subs,1)
     all_pow_miss = data_sub_power(trial_ind(i).miss,:,:,:);
     % effect sizes calculation (mean and var)
     % voltage
-    voltage_hit_miss_ds(i,:,:) = mean(voltage_hit,1) - mean(voltage_miss,1);  
+    voltage_hit_miss_ds(i,:,:) = mean(voltage_hit,1) - mean(voltage_miss,1);
     voltage_hit_miss_vars(i,:,:) = var(voltage_hit,0,1)./ size(voltage_hit,1)+ ...
-        var(voltage_miss,0,1) ./ size(voltage_miss,1); 
+        var(voltage_miss,0,1) ./ size(voltage_miss,1);
     % pow
    pow_hit_miss_ds(i,:,:, :) = mean(all_pow_hit,1) - mean(all_pow_miss,1);  % mean difference of both classes per subject
    pow_hit_miss_vars(i,:,:, :) = var(all_pow_hit,0,1) ./ size(all_pow_hit,1) + ...
-        var(all_pow_miss,0,1) ./ size(all_pow_miss,1); 
+        var(all_pow_miss,0,1) ./ size(all_pow_miss,1);
 end
 [p_volt_uncorr3, p_volt_fdr3, z_volt3] = group_analysis(voltage_hit_miss_ds,voltage_hit_miss_vars,num_channels,alpha);
 [p_pow_uncorr3, p_pow_fdr3, z_pow3]=group_analysis(pow_hit_miss_ds, pow_hit_miss_vars,num_channels,alpha);
@@ -146,13 +145,13 @@ for i = 1:size(subs,1)
     all_pow_forg = data_sub_power(trial_ind(i).forg,:,:,:);
     % effect sizes calculation (mean and var)
     % voltage
-    voltage_rem_forg_ds(i,:,:) = mean(voltage_rem,1) - mean(voltage_forg,1);  
+    voltage_rem_forg_ds(i,:,:) = mean(voltage_rem,1) - mean(voltage_forg,1);
     voltage_rem_forg_vars(i,:,:) = var(voltage_rem,0,1)./ size(voltage_rem,1)+ ...
-        var(voltage_forg,0,1) ./ size(voltage_forg,1); 
+        var(voltage_forg,0,1) ./ size(voltage_forg,1);
     % pow
    pow_rem_forg_ds(i,:,:, :) = mean(all_pow_rem,1) - mean(all_pow_forg,1);  % mean difference of both classes per subject
    pow_rem_forg_vars(i,:,:, :) = var(all_pow_rem,0,1) ./ size(all_pow_rem,1) + ...
-        var(all_pow_forg,0,1) ./ size(all_pow_forg,1); 
+        var(all_pow_forg,0,1) ./ size(all_pow_forg,1);
 end
 [p_volt_uncorr4, p_volt_fdr4, z_volt4] = group_analysis(voltage_rem_forg_ds,voltage_rem_forg_vars,num_channels,alpha);
 [p_pow_uncorr4, p_pow_fdr4, z_pow4] = group_analysis(pow_rem_forg_ds, pow_rem_forg_vars,num_channels,alpha);
